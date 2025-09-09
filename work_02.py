10ffb7c26e28df2e8d0ee2673a413525b6576d32
Bosak777 <masafujurasaki1126@gmail.com>
2025-09-10 00:33:08 +0900

    反射神経

--- work_02.py
+++ work_02.py
@@ -8,10 +8,8 @@ print(dt_now)
 
 # MySQLサーバーへの接続情報
 conn = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    password='bosaklong',
-    database='python_reflexes'
+    host="localhost", user="root", password="bosaklong", 
+    database="python_reflexes"
 )
 
 # 接続確認
@@ -34,22 +32,35 @@ if time_diff < 0.01:
     pass
 else:
     print(f"あなたの反応速度は:{timediff}秒です。")
+plnm = input("プレイヤー名を入力してください")
 # id,カラム名　record_time(何秒なのか),started_at(いつやったのか)
+# プレイヤーの名前を入れる(カラム名　player_name)
 # 作成するテーブル名　work2_py
 cursor = conn.cursor()
 # データ挿入のリクエスト
-insert_query = "insert into work2_py(id, record_time) values(%s,%s)"
-data = (None, timediff)
+insert_query = "insert into work2_py(record_time, player_name) "
+"values(%s, %s)"
+data = (timediff, plnm)
 #  クエリ実装
 cursor.execute(insert_query, data)
 #  変更を反映
 conn.commit()
 cursor.execute("select * from work2_py")
 rows = cursor.fetchall()
+cursor.execute("select * from work2_py order by record_time asc limit 5")
+asc = cursor.fetchall()
+cursor.execute("select * from work2_py order by record_time asc limit 1")
+bast = cursor.fetchall()
 # 結果の表示
 for row in rows:
     print(row)
-#  データを削除して１からかいし
+print("========ここから上位5人です=========")
+for a in asc:
+    print(a)
+print("========ここから最速記録です=========")
+for b in bast:
+    print(b)
+#  データを削除して１から開始
 # cursor.execute("truncate table work2_py")
 
 # cursor.close()
