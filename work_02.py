d8da1297b4c18f5612176f26122b9448d4b4411d
Bosak777 <masafujurasaki1126@gmail.com>
2025-09-10 00:31:55 +0900

    反射神経

--- work_02.py
+++ work_02.py
@@ -1,5 +1,23 @@
 import time
 import random
+import mysql.connector
+import datetime
+
+dt_now = datetime.datetime.now()
+print(dt_now)
+
+# MySQLサーバーへの接続情報
+conn = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='bosaklong',
+    database='python_reflexes'
+)
+
+# 接続確認
+if conn.is_connected():
+    print("MySQLサーバーに接続成功！")
+
 
 print("合図が出たらenterを押す")
 
@@ -16,3 +34,25 @@ if time_diff < 0.01:
     pass
 else:
     print(f"あなたの反応速度は:{timediff}秒です。")
+# id,カラム名　record_time(何秒なのか),started_at(いつやったのか)
+# 作成するテーブル名　work2_py
+cursor = conn.cursor()
+# データ挿入のリクエスト
+insert_query = "insert into work2_py(id, record_time) values(%s,%s)"
+data = (None, timediff)
+#  クエリ実装
+cursor.execute(insert_query, data)
+#  変更を反映
+conn.commit()
+cursor.execute("select * from work2_py")
+rows = cursor.fetchall()
+# 結果の表示
+for row in rows:
+    print(row)
+#  データを削除して１からかいし
+# cursor.execute("truncate table work2_py")
+
+# cursor.close()
+# conn.close()
+
+# print("データをリセットしました！（AUTO_INCREMENTも1から）")
